08f0264ef8cd8fdc5711d450760dc06d568231e7
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-12 19:29:30 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,6 @@ def game_hash
       team_name: "Brooklyn Nets",
       colors: ["Black","White"],
       players: [
-        
         {
           player_name:"Alan Anderson",
           number: 0,
