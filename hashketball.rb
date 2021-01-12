f945d09d7760e6cbe8e64221863d42b8c8a99705
Matthew Kindt <matthewkindt96@yahoo.com>
2021-01-12 19:10:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,14 +1,20 @@
 def game_hash
   {
- = {
-  :home => {
-    :team_name => [],
-    :colors => [],
-    :players => []
-  },
-:away => {
-  :team_name => [],
-  :colors => [],
-  :players => []
-}
-}
+    home: {
+      team_name: "",
+      colors: ["",""],
+      players: [
+        {
+          player_name:"",
+          number:,
+          shoe:,
+          points:,
+          rebounds:,
+          assists:,
+          steals:,
+          blocks:,
+          slam_dunks:
+        }
+        ]
+      },
+      
